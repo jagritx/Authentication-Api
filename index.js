4f056f319e88385c0a40cc73c5cdd52ee631e658
Jagrit Kamra <63954010+jagritx@users.noreply.github.com>
2021-10-20 06:10:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 //Importing routes
+/*
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts')
+*/
 const testRoute = require('./routes/test')
 
-
+/*
 dotenv.config();
-
+*/
 //Connect to DB
+/*
 mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true}, 
 () => console.log('Connected to db!'));
+*/
 
 //Middleware
 app.use(express.json());
 
 //Route Middlewares
+/*
 app.use('/api/user',authRoute);
 app.use('/api/posts',postRoute);
+*/
 app.use('/',testRoute);
 
 
